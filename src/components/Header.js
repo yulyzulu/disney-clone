96100d8645628b713f9c9c44fb5d06b57df45b68
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-10-19 22:49:33 -0500

    Added menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,22 @@ const Header = (props) => {
           <img src="/images/home-icon.svg" alt="Home icon" />
           <span>HOME</span>
         </a>
+        <a href="/search">
+          <img src="/images/search-icon.svg" alt="search icon" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/watch">
+          <img src="/images/watchlist-icon.svg" alt="watchlist icon" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/movies">
+          <img src="/images/movie-icon.svg" alt="movie icon" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/series">
+          <img src="/images/series-icon.svg" alt="series icon" />
+          <span>SERIES</span>
+        </a>
       </NavMenu>
     </Nav>
   );
@@ -57,9 +73,55 @@ const NavMenu = styled.div`
   margin-right: auto;
   margin-left: 25px;
 
-  @media (max-width: 768px) {
-    display: none;
-  }
-`;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
 
-export default Header;
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+  // @media (max-width: 768px) {
+  //   display: none;
+  // }
+  `;
+
+  export default Header;
